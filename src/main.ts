c9105bd633d83b5e629b538df6713ef0a2c5fca1
tai-kun <taikun.dev@gmail.com>
2024-10-25 15:11:13 +0900

    修正

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
+import { getInput, setFailed } from "@actions/core";
 import { execSync } from "node:child_process";
-import * as crypto from "node:crypto";
 import * as fs from "node:fs";
 import * as os from "node:os";
 import * as path from "node:path";
@@ -10,10 +9,9 @@ import CONFIG_YML from "./config.yml.js";
 main({
   version: getInput("version") || "latest",
   users: getInput("users"),
-  addr: getInput("addr") || "0.0.0.0:5000",
+  port: getInput("port") || "5000",
 })
-  .then(({ pid }) => {
-    setOutput("pid", pid);
+  .then(() => {
     process.exit(0);
   })
   .catch(err => {
@@ -25,7 +23,7 @@ async function main(
   inputs: {
     version: string;
     users: string;
-    addr: string;
+    port: string;
   },
 ) {
   const users = inputs.users.split(",").filter(Boolean).map(v => ({
@@ -45,38 +43,31 @@ async function main(
   fs.mkdirSync(carootdir, { recursive: true });
   fs.mkdirSync(configdir, { recursive: true });
 
-  // {
-  //   execSync(`sudo apt-get update -y`, { stdio: "inherit" });
-  //   execSync(`sudo apt-get install -y coreutils`, { stdio: "inherit" });
-  //   execSync(`. ~/.bashrc`, { stdio: "inherit" });
-  // }
-  {
+  run("htpasswd を用意", () => {
     const htpasswd = generateHtpasswdString(users);
-    const configYml = CONFIG_YML
-      .replace(/{{addr}}/g, inputs.addr)
-      .replace(/{{data}}/g, datadir)
-      .replace(/{{auth}}/g, authdir)
-      .replace(/{{certs}}/g, certsdir);
     fs.writeFileSync(path.join(authdir, "htpasswd"), htpasswd);
-    fs.writeFileSync(path.join(configdir, "config.yml"), configYml);
     console.log("htpasswd:");
     console.log(htpasswd);
+    console.log();
+  });
+
+  run("config.yml を用意", () => {
+    const configYml = CONFIG_YML
+      .replace(/{{addr}}/g, ":5000")
+      .replace(/{{data}}/g, "/var/lib/registry")
+      .replace(/{{auth}}/g, "/auth")
+      .replace(/{{certs}}/g, "/certs");
+    fs.writeFileSync(path.join(configdir, "config.yml"), configYml);
     console.log("config.yml:");
     console.log(configYml);
-  }
+    console.log();
+  });
 
-  const mkcert = JSON.stringify(
-    await downloadMkcert("linux", "amd64", "1.4.4"),
-  );
-  const registry = JSON.stringify(
-    await downloadRegistry("linux", "amd64", inputs.version),
-  );
-
-  {
+  await run("証明書を用意", async () => {
+    const mkcert = JSON.stringify(
+      await downloadMkcert("linux", "amd64", "1.4.4"),
+    );
     execSync(`chmod u+x ${mkcert}`, { stdio: "inherit" });
-    execSync(`chmod u+x ${registry}`, { stdio: "inherit" });
-  }
-  {
     execSync(`${mkcert} -install`, {
       stdio: "inherit",
       env: {
@@ -97,28 +88,50 @@ async function main(
         },
       },
     );
-  }
-  {
-    const configFile = JSON.stringify(path.join(configdir, "config.yml"));
-    const out = execSync(
-      `${registry} serve ${configFile} &\necho $!`,
+  });
+
+  await run("registry を起動", async () => {
+    // https://github.com/distribution/distribution/issues/4270
+    execSync(
+      `docker run \\
+      -d \\
+      --restart=always \\
+      --name registry \\
+      -p ${inputs.port}:5000
+      -e OTEL_TRACES_EXPORTER=none \\
+      -v ${JSON.stringify(authdir + ":/auth")} \\
+      -v ${JSON.stringify(certsdir + ":/certs")} \\
+      -v ${JSON.stringify(configdir + ":/config")} \\
+      registry:${inputs.version}
+      /config/config.yml
+      `,
       {
-        encoding: "utf-8",
-        stdio: "pipe",
-        env: {
-          ...process.env,
-          // https://github.com/distribution/distribution/issues/4270
-          OTEL_TRACES_EXPORTER: "none",
-        },
+        stdio: "inherit",
       },
     );
-    await setTimeout(3e3);
-    const pid = /(\d+)\s*$/.exec(out)![1]!;
+    await setTimeout(1e3);
+  });
+}
 
-    return {
-      pid,
-    };
+function run<T>(title: string, cb: () => T): T {
+  console.log("::group::" + title);
+  let r;
+
+  try {
+    r = cb();
+  } finally {
+    if (!(r instanceof Promise)) {
+      console.log("::endgroup::");
+    }
   }
+
+  if (r instanceof Promise) {
+    r = r.finally(() => {
+      console.log("::endgroup::");
+    }) as T;
+  }
+
+  return r;
 }
 
 async function downloadMkcert(
@@ -128,74 +141,26 @@ async function downloadMkcert(
 ): Promise<string> {
   const url = "https://github.com/FiloSottile/mkcert/releases/download/v"
     + version + "/mkcert-v" + version + "-" + os + "-" + arch;
-  execSync("wget -O mkcert " + url, { stdio: "inherit" });
+  execSync("wget -q -O mkcert " + url, { stdio: "inherit" });
 
   return path.join(process.cwd(), "mkcert");
-}
-
-async function downloadRegistry(
-  os: string,
-  arch: string,
-  version: string,
-): Promise<string> {
-  let url = "https://api.github.com/repos/distribution/distribution/releases/";
-
-  if (version === "latest") {
-    url += "latest";
-  } else {
-    if (version[0] === "v") {
-      version = version.slice(1);
-    }
-
-    url += "tag/" + version;
-  }
-
-  const resp = await fetch(url, {
-    headers: {
-      "Accept": "application/vnd.github.v3+json",
-    },
-  });
-
-  if (!resp.ok) {
-    throw new Error(resp.status + " " + resp.statusText);
-  }
-
-  const { assets } = await resp.json() as {
-    assets: {
-      name: string;
-      browser_download_url: string;
-    }[];
-  };
-
-  for (const { name, browser_download_url: url } of assets) {
-    if (!name.endsWith("_" + os + "_" + arch + ".tar.gz")) {
-      continue;
-    }
-
-    execSync("wget -O registry.tar.gz " + url, { stdio: "inherit" });
-    execSync("tar -xzvf registry.tar.gz registry", { stdio: "inherit" });
-    execSync("rm registry.tar.gz", { stdio: "inherit" });
-    break;
-  }
-
-  return path.join(process.cwd(), "registry");
 }
 
 function generateHtpasswdString(
   users: { username: string; password: string }[],
 ): string {
-  const htpasswdLines = users.map(user => {
-    const hashedPassword = generateMD5Hash(user.password);
+  const htpasswdLines = users.map(u => {
+    const user = JSON.stringify(u.username);
+    const pass = JSON.stringify(u.password);
 
-    return user.username + ":{MD5}" + hashedPassword;
+    return execSync(
+      `docker run --rm --entrypoint htpasswd httpd:2 -Bbn ${user} ${pass}`,
+      {
+        encoding: "utf-8",
+        stdio: "pipe",
+      },
+    ).trim();
   });
 
   return htpasswdLines.join("\n");
-}
-
-function generateMD5Hash(password: string): string {
-  const hash = crypto.createHash("md5");
-  hash.update(password);
-
-  return hash.digest("hex");
 }
